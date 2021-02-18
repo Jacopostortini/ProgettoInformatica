2c8dea4237d9f79d263595cc6bf8ce93a5765ec1
tonma <tonmasodelucchhi@gmail.com>
2021-02-18 16:50:55 +0100

    ciao

---
+++ src/main/java/com/mycompany/progettoinfo/Influenza.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.progettoinfo;
+
+/**
+ *
+ * @author tonma
+ */
+public class Influenza extends Personaggio {
+
+    @Override
+    public int getForza() {
+        return this.esperienza*4+20; //L'esperienza dell'influenza l'ho inventata io e l'ho impostata come la più debole visto che i componenti del male nel gioco sono 3 e noi ne abbiamo messi 4
+        
+    }
+    
+}
