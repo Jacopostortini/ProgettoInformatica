7aad302c4136d6687e6a4a979ba752fef621fe65
tonma <tonmasodelucchhi@gmail.com>
2021-02-18 16:51:11 +0100

    ciao

---
+++ src/main/java/com/mycompany/progettoinfo/Covid.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.progettoinfo;
+
+/**
+ *
+ * @author tonma
+ */
+public class Covid extends Personaggio {
+
+    @Override
+    public int getForza() {
+        if (this.esperienza<5){
+            return this.esperienza*2+30;
+        }
+        else{
+            return this.esperienza*3+70; //Il covid l'ho paragonato agli Orchi del gioco originale
+        }
+    }
+    
+    
+}
