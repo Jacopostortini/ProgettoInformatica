114aa573267cfd9179e64bc599cec7d907d12c13
tonma <tonmasodelucchhi@gmail.com>
2021-02-18 16:51:26 +0100

    ciao

---
+++ src/main/java/com/mycompany/progettoinfo/AngelaDaMondello.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.progettoinfo;
+
+/**
+ *
+ * @author tonma
+ */
+public class AngelaDaMondello extends Personaggio {
+
+    @Override
+    public int getForza() {
+        return this.esperienza*5+40; //Angela Da Mondello l'ho paragonata ai Sudroni del gioco originale
+    }
+    
+    
+}
